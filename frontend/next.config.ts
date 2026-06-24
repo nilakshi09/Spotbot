aad849eef887126b8611df280850bd6082658aa7
Nilakshi Rahangdale <nilakshirahangdale@gmail.com>
2026-06-24 10:59:40 +0530

    fix sentry config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,35 @@
+// import type { NextConfig } from "next";
+// import { withSentryConfig } from '@sentry/nextjs';
+
+// const nextConfig: NextConfig = {};
+
+// export default withSentryConfig(nextConfig, {
+//   org: 'spotbot',
+//   project: 'spotbot-frontend',
+
+//   // Upload source maps to Sentry for readable stack traces
+//   silent: true,
+//   widenClientFileUpload: true,
+//   hideSourceMaps: true,
+//   disableLogger: true,
+
+//   // Automatically tree-shake Sentry logger in production
+//   automaticVercelMonitors: false,
+// });
+
+
+
+
+
 import type { NextConfig } from "next";
-import { withSentryConfig } from '@sentry/nextjs';
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {};
 
 export default withSentryConfig(nextConfig, {
-  org: 'spotbot',
-  project: 'spotbot-frontend',
+  org: "spotbot",
+  project: "spotbot-frontend",
 
-  // Upload source maps to Sentry for readable stack traces
   silent: true,
   widenClientFileUpload: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-
-  // Automatically tree-shake Sentry logger in production
-  automaticVercelMonitors: false,
 });
